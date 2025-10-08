1bfe364401556930c039475dadd2fb419c98cf6f
yllcakiqi1 <cakiqiyll7@gmail.com>
2025-10-08 17:21:46 +0200

    Update projekt.sql

--- projekt.sql
+++ projekt.sql
@@ -1,9 +1,21 @@
+CREATE DATABASE appointment_db;
+
+USE appointment_db;
+
+CREATE TABLE appointments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    note TEXT
+);
+
 CREATE TABLE admin_users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL -- hashed password
+    password VARCHAR(255) NOT NULL
 );
 
--- Insert default admin user (password = "admin123")
 INSERT INTO admin_users (username, password)
 VALUES ('admin', SHA2('admin123', 256));
