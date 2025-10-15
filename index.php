1457149340872aecf154c98a370e5c8a0d655ed6
yllcakiqi1 <cakiqiyll7@gmail.com>
2025-10-15 15:49:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,21 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+<nav class="navbar">
+    <div class="logo"><a href="index.php">AppointmentPro</a></div>
+    <div class="nav-links">
+        <a href="index.php">Home</a>
+        <a href="book.php">Book Appointment</a>
+        <a href="login.php">Admin</a>
+        <a href="https://twitter.com" target="_blank">Twitter</a>
+        <a href="https://instagram.com" target="_blank">Instagram</a>
+        <a href="https://facebook.com" target="_blank">Facebook</a>
+    </div>
+</nav>
+
 <div class="container">
-    <h1>Welcome to the Appointment System</h1>
+    <h1>Welcome to AppointmentPro</h1>
     <p>Choose how you want to continue:</p>
     <div class="buttons">
         <a href="login.php"><button>Admin Login</button></a>
@@ -14,5 +27,13 @@
     </div>
     <p>Guest credentials: <b>username: guest</b>, <b>password: guest123</b></p>
 </div>
+
+<footer>
+    <p>© 2025 AppointmentPro. All rights reserved.</p>
+    <a href="https://twitter.com" target="_blank">Twitter</a>
+    <a href="https://instagram.com" target="_blank">Instagram</a>
+    <a href="https://facebook.com" target="_blank">Facebook</a>
+</footer>
+
 </body>
 </html>
